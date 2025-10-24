3496f1d7fe81d36536ada1e1dcdbba11a0aaf6aa
Ali Ahangar <aliahangar80@gmail.com>
2025-10-24 17:06:02 +0330

    modularize code and fix typo in exchange_rates dict

--- currency_convertor.py
+++ currency_convertor.py
@@ -1,33 +1,31 @@
-currencies = ("eur", "usd", "cad")
+def get_amount():
+    while True:
+        try:
+            amount = float(input("Enter the amount: "))
+            if amount <= 0:
+                raise ValueError()
+            return amount
+        except ValueError:
+            print("Invalid input")
+
+def get_currency(label):
+    currencies = ("eur", "usd", "cad")
+    while True:
+        currency = input(f"{label} currency (USD/EUR/CAD): ").lower()
+        if currency not in currencies:
+            print("Invalid input")
+        else:
+            return currency
+
+amount = get_amount()
+source_currency = get_currency("Source")
+target_currency = get_currency("Target")
 
 exchange_rates = {
     "usd": {"eur":0.85 , "cad":1.25 },
     "eur": {"usd":1.18 , "cad":1.47 },
-    "cad": {"usd":0.80 , "cad":0.68 },
+    "cad": {"usd":0.80 , "eur":0.68 },
 }
-
-while True:
-    try:
-        amount = float(input("Enter the amount: "))
-        if amount <= 0:
-            raise ValueError()
-        break
-    except ValueError:
-        print("Invalid input")
-
-while True:
-    source_currency = input("Source currency (USD/EUR/CAD): ").lower()
-    if source_currency not in currencies:
-        print("Invalid input")
-    else:
-        break
-
-while True:
-    target_currency = input("Target currency (USD/EUR/CAD): ").lower()
-    if target_currency not in currencies:
-        print("Invalid input")
-    else:
-        break
 
 if source_currency == target_currency:
     converted_amount = amount
